41d893696c8b1e325f5fc6571b1f88b4e23b7184
rohitrox444 <rohitrox444@gmail.com>
2020-06-05 23:32:00 +0530

    Final Submission

---
+++ RohitKumar/apps/common/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView
+from .forms import  SignUpForm
+from  django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'common/home.html'
+
+class DashboardView(LoginRequiredMixin,TemplateView):
+    template_name = 'common/dashboard.html'
+    login_url = reverse_lazy('home')
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('home')
+    template_name = 'common/register.html'
+
+
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .forms import UserForm, ProfileForm
+from django.contrib.auth.models import User
+from apps.userprofile.models import Profile
+
+from django.contrib import messages
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'common/profile.html'
+
+class ProfileUpdateView(LoginRequiredMixin, TemplateView):
+    user_form = UserForm
+    profile_form = ProfileForm
+    template_name = 'common/profile-update.html'
+
+    def post(self, request):
+
+        post_data = request.POST or None
+        file_data = request.FILES or None
+
+        user_form = UserForm(post_data, instance=request.user)
+        profile_form = ProfileForm(post_data, file_data, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully!')
+            return HttpResponseRedirect(reverse_lazy('profile'))
+
+        context = self.get_context_data(
+                                        user_form=user_form,
+                                        profile_form=profile_form
+                                    )
+
+        return self.render_to_response(context)
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
---
+++ RohitKumar/cloudtitantest/local_settings.py
@@ -0,0 +1,19 @@
+
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cloudtitan',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
